f4d8b1af504700befa5216c1976dcd506d0623f6
Lossy <thedragon1012@gmail.com>
2021-02-21 02:07:47 -0600

    Refactor InputDialog.kt

--- app/src/main/java/org/helllabs/android/xmp/browser/InputDialog.kt
+++ app/src/main/java/org/helllabs/android/xmp/browser/InputDialog.kt
@@ -7,20 +7,26 @@ import android.widget.EditText
 import android.widget.LinearLayout
 
 class InputDialog(context: Context) : AlertDialog.Builder(context) {
+
     val input: EditText
 
     init {
         val scale = context.resources.displayMetrics.density
-        val layout = LinearLayout(context)
         val pad = (scale * 6).toInt()
-        layout.setPadding(pad, pad, pad, pad)
-        input = EditText(context)
-        input.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-        input.transformationMethod = SingleLineTransformationMethod()
-        layout.addView(input)
-        setView(layout)
+
+        input = EditText(context).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            transformationMethod = SingleLineTransformationMethod()
+        }
+
+        LinearLayout(context).apply {
+            setPadding(pad, pad, pad, pad)
+            addView(input)
+        }.also {
+            setView(it)
+        }
     }
 }
