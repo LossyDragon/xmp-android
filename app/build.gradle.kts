2900a84721563aa5c279bce5c41c819d9f77a65a
LossyDragon <thedragon1012@gmail.com>
2026-05-18 11:25:45 -0500

    try again?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         minSdk = 24 // Android 7 - Nougat
         targetSdk = 36 // Android 15 Vanilla Ice Cream
 
-        versionCode = 129
+        versionCode = 130
         versionName = "5.0-SNAPSHOT"
 
         ndk.abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
